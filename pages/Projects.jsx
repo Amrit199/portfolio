b94d62934f7c79ecb9d8455e2c965b259af70185
Amrit <tamangamrit37@gmail.com>
2023-07-06 22:45:27 +0200

    update

--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -6,60 +6,110 @@ import weather from "../public/assets/projects/weather.png";
 import wordpressportfolio from "../public/assets/projects/wordpressportfolio.png";
 import webstudio from "../public/assets/projects/webstudio.png";
 import ProjectCard from "../components/ProjectCard";
-import { AnimatePresence, motion } from "framer-motion";
+import { Tab } from "@headlessui/react";
 
-export const porjectsImg = [
-  {
-    img: accessiNor,
-    name: "AccessiNor",
-    link: "/AccessiNor",
-  },
-  {
-    img: gallery,
-    name: "Image Gallery",
-    link: "/Gallery",
-  },
-  {
-    img: momo,
-    name: "MOMO",
-    link: "/Momo",
-  },
-  {
-    img: weather,
-    name: "Today's Weather",
-    link: "/Weather",
-  },
-  {
-    img: wordpressportfolio,
-    name: "Portfolio Wordpress",
-    link: "/Portfolio",
-  },
-  {
-    img: webstudio,
-    name: "Web Studio",
-    link: "/Webstudio",
-  },
-];
+const classNames = (...classes) => {
+  return classes.filter(Boolean).join(" ");
+};
+
+export const porjectsImg = {
+  categories: [
+    {
+      id: "startUps",
+      name: "StartUps",
+      projects: [
+        {
+          img: accessiNor,
+          name: "AccessiNor",
+          link: "/AccessiNor",
+        },
+        {
+          img: webstudio,
+          name: "Web Studio",
+          link: "/Webstudio",
+        },
+      ],
+    },
+    {
+      id: "portfolio",
+      name: "portfolio",
+      projects: [
+        {
+          img: gallery,
+          name: "Image Gallery",
+          link: "/Gallery",
+        },
+        {
+          img: weather,
+          name: "Today's Weather",
+          link: "/Weather",
+        },
+      ],
+    },
+    {
+      id: "ecommerce",
+      name: "E-commerce",
+      projects: [
+        {
+          img: momo,
+          name: "MOMO",
+          link: "/Momo",
+        },
+      ],
+    },
+    {
+      id: "cms",
+      name: "CMS",
+      projects: [
+        {
+          img: wordpressportfolio,
+          name: "Portfolio Wordpress",
+          link: "/Portfolio",
+        },
+      ],
+    },
+  ],
+};
 
 const Projects = () => {
   return (
-    <AnimatePresence>
-      <div id="Projects" className="w-full h-full pt-12">
-        <h2 className="text-center my-4">PROJECTS</h2>
-        <div className="w-[90%] mx-auto bg-white p-8 flex items-center justify-center gap-14 flex-wrap">
-          {porjectsImg.map((item, index) => (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 2, delay: 0.5 }}
-              key={index}
+    <div id="Projects" className="w-full h-full py-12">
+      <h2 className="text-center">PROJECTS</h2>
+      <Tab.Group>
+        <Tab.List className="w-[80%] mx-auto flex items-center justify-center gap-12 rounded-xl p-4">
+          {porjectsImg.categories.map((category) => (
+            <Tab
+              key={category.name}
+              className={({ selected }) =>
+                classNames(
+                  "rounded-lg py-2 text-sm font-medium",
+                  "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+                  selected
+                    ? "bg-white shadow"
+                    : "text-gray-600 hover:text-black"
+                )
+              }
             >
-              <ProjectCard item={item} />
-            </motion.div>
+              {category.name}
+            </Tab>
           ))}
-        </div>
-      </div>
-    </AnimatePresence>
+        </Tab.List>
+        <Tab.Panels>
+          {porjectsImg.categories.map((posts, idx) => (
+            <Tab.Panel
+              key={idx}
+              className={classNames(
+                "w-[80%] h-full mx-auto rounded-2xl shadow-2xl py-28 bg-white flex items-center justify-center gap-14 flex-wrap"
+              )}
+            >
+              {posts.projects.map((post) => (
+                <ProjectCard item={post} />
+              ))}
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,8 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    // ...
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
