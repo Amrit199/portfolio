6062456d9cd71ca41e2ce3dd611296feaa06c46d
Amrit Tamang <44949877+Amrit199@users.noreply.github.com>
2023-12-12 16:25:19 +0100

    update

---
+++ emailjs.cli.js
@@ -0,0 +1,4 @@
+
+export const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID
+export const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID
+export const PublicID = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_ID
--- pages/Contact.jsx
+++ pages/Contact.jsx
@@ -1,101 +1,108 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import Socialcontacts from "../components/Socialcontacts";
 import { Data } from "../data";
-import CustomInput from "../components/CustomInput";
-import CustomButton from "../components/CustomButton";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import emailjs from "@emailjs/browser";
+import { PublicID, serviceID, templateID } from "../emailjs.cli";
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    mobile: "",
-    email: "",
-    subject: "",
-    message: "",
-  });
-  const [error, setError] = useState(false);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+  const form = useRef();
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (
-      (formData.name,
-      formData.mobile,
-      formData.email,
-      formData.subject,
-      formData.message == "")
-    ) {
-      alert("Please Fill out all the details");
-      // return setError(!error)
-    } else {
-      alert("Thank you, your message has been sent");
-      setFormData({
-        name: "",
-        mobile: "",
-        email: "",
-        subject: "",
-        message: "",
+  const sendEmail = async () => {
+    try {
+      await emailjs.sendForm(serviceID, templateID, form.current, PublicID);
+      Swal.fire({
+        title: "Takk for medling",
+        text: "Vi har mottatt meldingen din og kommer tilbake til deg så snart som mulig",
+        icon: "success",
       });
+      reset();
+    } catch (error) {
+      console.error(error.text);
     }
   };
   return (
     <div id="Contact" className="w-full h-full pt-20 mb-8">
-      <h1 className="text-center">CONTACT ME</h1>
-      <div className="w-full text-center p-4 flex items-center justify-center gap-4 relative">
-        {Data.socialicons.map((item, index) => (
-          <Socialcontacts item={item} key={index} />
-        ))}
-      </div>
-      <form onSubmit={handleSubmit}>
-        <div className="w-[95%] md:w-[60%] lg:w-[50%] rounded-lg p-4 shadow-xl shadow-gray-600 mx-auto flex flex-col items-center justify-between gap-4">
-          {/* {error ? <h2>Please fill the form</h2> : ""} */}
-          <div className="w-full flex flex-col flex-wrap items-center justify-between gap-4">
-            <CustomInput
-              title="Name"
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-            <CustomInput
-              title="Mobile Number"
-              type="text"
-              name="mobile"
-              value={formData.mobile}
-              onChange={handleChange}
-            />
-            <CustomInput
-              title="Email"
-              type="email"
-              value={formData.email}
-              name="email"
-              onChange={handleChange}
-            />
-            <CustomInput
-              title="Subject"
-              type="text"
-              value={formData.subject}
-              name="subject"
-              onChange={handleChange}
-            />
-            <div className="w-full flex flex-col gap-1">
-              <label>MESSAGE</label>
-              <textarea
-                className="p-3 border-2 rounded-lg"
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-              ></textarea>
-            </div>
-            <CustomButton title="Send" type="submit" className="w-full text-lg font-medium bg-white hover:bg-green-50"/>
+      <h1 className="text-center py-3">CONTACT ME</h1>
+      <div className="w-[90%] sm:w-[80%] md:w-[65%] lg:w-[50%] mx-auto bg-slate-200 p-6 rounded-xl">
+        <div className="flex flex-col items-center justify-center gap-2 py-4">
+          <h4 className="text-center">Contact me through social media</h4>
+          <div className="w-full text-center p-4 flex items-center justify-center gap-4 relative">
+            {Data.socialicons.map((item, index) => (
+              <Socialcontacts item={item} key={index} />
+            ))}
           </div>
+          <h4 className="text-center">Or through this contact form</h4>
         </div>
-      </form>
+        <form
+          ref={form}
+          onSubmit={handleSubmit(sendEmail)}
+          className="w-full flex flex-col items-start justify-center gap-3"
+        >
+          <div className="w-full flex flex-col gap-1">
+            <label>Name</label>
+            <input
+              {...register("user_name", { required: true })}
+              aria-invalid={errors.user_name ? "true" : "false"}
+              type="text"
+              name="user_name"
+              placeholder="Ditt fulle navn"
+              className="p-3 border-2 rounded-lg font-normal"
+            />
+            {errors.user_name?.type === "required" && (
+              <p role="alert" className="text-red-700">
+                Please fill up the name
+              </p>
+            )}
+          </div>
+          <div className="w-full flex flex-col gap-1">
+            <label>Email address</label>
+            <input
+              {...register("user_email", { required: true })}
+              aria-invalid={errors.user_email ? "true" : "false"}
+              type="email"
+              name="user_email"
+              placeholder="Din e-postadresse"
+              className="p-3 border-2 rounded-lg font-normal"
+            />
+            {errors.user_email?.type === "required" && (
+              <p role="alert" className="text-red-700">
+                Email is required
+              </p>
+            )}
+          </div>
+          <div className="w-full flex flex-col gap-1">
+            <label>Message</label>
+            <textarea
+              {...register("message", { required: true })}
+              aria-invalid={errors.message ? "true" : "false"}
+              name="message"
+              rows={5}
+              cols={40}
+              placeholder="Skriv en medling her..."
+              className="p-3 border-2 rounded-lg font-normal"
+            />
+            {errors.message?.type === "required" && (
+              <p role="alert" className="text-red-700">
+                Message is required
+              </p>
+            )}
+          </div>
+          <button
+            type="submit"
+            className="w-full py-4 bg-[#254291] text-white rounded-lg text-lg font-semibold hover:bg-[#1f1f1f] transition-all"
+          >
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
